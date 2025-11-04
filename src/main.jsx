7ca0b6be4dd47eab3061fcbe5e0cab8416f9b69e
unknown <mdtamimhossain1357@gmail.com>
2025-11-04 08:58:59 +0600

    added login and signup with firebase auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "./routes/Routes";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App></App>
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
 );
